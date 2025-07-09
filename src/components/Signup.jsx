9b3ee1b98a7b2d39a42584c45e0199ed2fa8d80e
Gitali03 <aonermtk@gmail.com>
2025-07-09 20:03:54 +0300

    validating the passwords

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -1,10 +1,21 @@
+import { useState } from "react";
+
 export default function Signup() {
+
+  const [passwordsAreNotEqual, setPasswordsAreNotEqual] = useState(false);
   function handleSubmit(event) {
     event.preventDefault();
     const fd = new FormData(event.target);
     const acquisitionChannel = fd.getAll("acquisition");
     const data = Object.fromEntries(fd.entries());
     data.acquisition = acquisitionChannel;
+
+    if (data.password !== data ['confirm-password']) {
+      setPasswordsAreNotEqual(true);
+      return;
+    }
+
+
     console.log(data);
   }
   return (
@@ -33,6 +44,7 @@ export default function Signup() {
             id="confirm-password"
             name="confirm-password"
           />
+          <div className="control-error">{passwordsAreNotEqual && <p>Passwords must match</p>}</div>
         </div>
       </div>
       <hr />
--- src/components/StateLogin.jsx
+++ src/components/StateLogin.jsx
@@ -4,17 +4,17 @@ export default function StateLogin() {
   //const [enteredEmail, setEnteredEmail] = useState('');
   //const [eneteredPassword,setEnteredPassword] = useState('');
 
-  const [enteredValues, setEnteredValues] = useState ({
-    email: '',
-    password: ''
-  });
-  
-  const [didEdit, setDidEdit] = useState({
-    email:false,
-    password:false
+  const [enteredValues, setEnteredValues] = useState({
+    email: "",
+    password: "",
   });
 
-  const emailIsInvalid = didEdit.email && !enteredValues.email.includes('@');
+  const [didEdit, setDidEdit] = useState({
+    email: false,
+    password: false,
+  });
+
+  const emailIsInvalid = didEdit.email && !enteredValues.email.includes("@");
 
   function handleSubmit(event) {
     event.preventDefault();
@@ -22,28 +22,28 @@ export default function StateLogin() {
   }
 
   function handleInputChange(identifier, value) {
-    setEnteredValues(prevValues => ({
+    setEnteredValues((prevValues) => ({
       ...prevValues,
-      [identifier]: value
+      [identifier]: value,
     }));
-    setDidEdit ((prevEdit) => ({
+    setDidEdit((prevEdit) => ({
       ...prevEdit,
       [identifier]: false,
-    }))
+    }));
   }
 
   function handleInputBlur(identifier) {
-    setDidEdit(prevEdit => ({
+    setDidEdit((prevEdit) => ({
       ...prevEdit,
-      [identifier]:true
+      [identifier]: true,
     }));
   }
 
- // function handleEmailChange(event) {
-   // setEnteredEmail(event.target.value);
+  // function handleEmailChange(event) {
+  // setEnteredEmail(event.target.value);
   //}
- // function handlePasswordChange(event) {
-   // setEnteredPassword(event.target.value);
+  // function handlePasswordChange(event) {
+  // setEnteredPassword(event.target.value);
   //}
   return (
     <form onSubmit={handleSubmit}>
@@ -52,13 +52,30 @@ export default function StateLogin() {
       <div className="control-row">
         <div className="control no-margin">
           <label htmlFor="email">Email</label>
-          <input id="email" type="email" name="email" onChange={(event) => handleInputChange('email', event.target.value)} value={enteredValues.email} onBlur={() => handleInputBlur('email')}/>
-          <div className="control-error">{emailIsInvalid && <p>Please enter a valid email addres.</p>}</div>
+          <input
+            id="email"
+            type="email"
+            name="email"
+            onChange={(event) => handleInputChange("email", event.target.value)}
+            value={enteredValues.email}
+            onBlur={() => handleInputBlur("email")}
+          />
+          <div className="control-error">
+            {emailIsInvalid && <p>Please enter a valid email addres.</p>}
+          </div>
         </div>
 
         <div className="control no-margin">
           <label htmlFor="password">Password</label>
-          <input id="password" type="password" name="password" onChange={(event) => handleInputChange('password', event.target.value)} value={enteredValues.password} />
+          <input
+            id="password"
+            type="password"
+            name="password"
+            onChange={(event) =>
+              handleInputChange("password", event.target.value)
+            }
+            value={enteredValues.password}
+          />
         </div>
       </div>
 
